4acf658932169a1bcb6701f81e182f3eef67911c
jin <dbrwls12@gmail.com>
2026-05-12 14:20:02 +0900

    [fix/#29] 마이페이지 Security 경로 누락으로 인한403 오류
    SecurityConfig에 마이페이지 경로 추가

--- blog/blog/src/main/java/com/example/demo/controller/member/MemberRestController.java
+++ blog/blog/src/main/java/com/example/demo/controller/member/MemberRestController.java
@@ -7,14 +7,12 @@ import com.example.demo.service.member.JwtTokenProvider;
 import com.example.demo.service.member.MemberService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
